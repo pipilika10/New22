b27da451244a0fc15ea2c49076f1153eb1250597
pipilika10 <paramitapp10@gmail.com>
2023-01-14 23:38:53 +0530

    updated

---
+++ javascript/dataTypes.js
@@ -0,0 +1,33 @@
+var x;
+var str="Hello World";
+let boolVal=true;
+
+console.log(typeof(boolVal));
+
+var z;
+var a=null;
+
+var obj={a:10};
+obj=null;
+
+//console.log(obj.a);
+
+
+var emp2=new Object();
+emp2.id=001;
+
+function Emp3(){
+
+}
+
+//arrays
+
+var arr=[1,"2",3];
+
+arr[3]=4;
+
+console.log("arr length:"+arr.length);
+
+for(var i=0;i<arr.length;i++){
+    console.log((arr[i]));
+}
